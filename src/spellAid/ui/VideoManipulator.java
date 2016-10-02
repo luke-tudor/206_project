690b9c9d87935dd7dbfe3c5ae2af2370e8485bc9
Luke Tudor <ltud719@aucklanduni.ac.nz>
2016-10-02 14:58:29 +1300

    added ability to manipulate video with ffmpeg, not blocking

--- src/spellAid/ui/VideoManipulator.java
+++ src/spellAid/ui/VideoManipulator.java
@@ -2,37 +2,73 @@ package spellAid.ui;
 
 import java.io.IOException;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Slider;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 
-public class VideoManipulator extends BorderPane implements EventHandler<ActionEvent> {
-
+public class VideoManipulator extends BorderPane {
+	
 	private Button submit;
+	
+	private ControlPanel cPanel;
 	
 	public VideoManipulator() {
 		super();
 		
 		submit = new Button("Submit");
-		submit.setOnAction(this);
+		submit.setOnAction(e -> createVideo());
 		
-		setCenter(submit);
-	}
-
-	@Override
-	public void handle(ActionEvent e) {
-		if (e.getSource() == submit) {
-			createVideo();
-		}
+		cPanel = new ControlPanel();
+		cPanel.setAlignment(Pos.CENTER);
+		
+		FlowPane flow = new FlowPane(submit);
+		flow.setAlignment(Pos.CENTER);
+		
+		setCenter(cPanel);
+		setBottom(flow);
+		setPadding(new Insets(5));
 	}
 	
 	private void createVideo() {
-		String options = "-vf negate";
-		String cmd = "ffmpeg -y -i big_buck_bunny_1_minute.avi " + options + " out.avi";
+		String options = cPanel.getSettings();
+		String cmd = "ffmpeg -y -i big_buck_bunny_1_minute.mp4 "+options+" -strict -2 out.mp4";
 		ProcessBuilder pb = new ProcessBuilder("bash", "-c", cmd);
 		try {
 			pb.start();
 		} catch (IOException e) {}
+	}
+	
+	private class ControlPanel extends GridPane {
+		
+		private RadioButton negate;
+		
+		private Slider fps;
+		
+		private ControlPanel() {
+			negate = new RadioButton("Negate");
+			fps = new Slider(1, 24, 24);
+			fps.setShowTickLabels(true);
+			
+			setVgap(5);
+			setHgap(5);
+			setPadding(new Insets(5));
+
+			add(negate, 0, 0);
+			add(fps, 1, 0);
+		}
+		
+		private String getSettings() {
+			StringBuilder sb = new StringBuilder();
+			if (negate.isSelected()) {
+				sb.append("-vf negate");
+			}
+			sb.append(" -r " + fps.getValue());
+			return sb.toString();
+		}
 	}
 }
