79fe4704dece15b5e855e31c433b9a97e93c180b
Luke Tudor <ltud719@aucklanduni.ac.nz>
2016-10-02 17:38:28 +1300

    negation now works with text

--- src/spellAid/ui/VideoManipulator.java
+++ src/spellAid/ui/VideoManipulator.java
@@ -13,35 +13,36 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 
 public class VideoManipulator extends BorderPane {
-	
+
 	private Button submit;
-	
+
 	private ControlPanel cPanel;
-	
+
 	public VideoManipulator() {
 		super();
-		
+
 		submit = new Button("Submit");
 		submit.setOnAction(e -> createVideo());
-		
+
 		cPanel = new ControlPanel();
 		cPanel.setAlignment(Pos.CENTER);
-		
+
 		setCenter(cPanel);
 		setBottom(submit);
 		setPadding(new Insets(5));
 	}
-	
+
 	private void createVideo() {
-		
+
 		submit.setDisable(true);
-		
+
 		Thread worker = new Thread(new Runnable() {
 
 			@Override
 			public void run() {
 				String options = cPanel.getSettings();
-				String cmd = "ffmpeg -y -i big_buck_bunny_1_minute.mp4"+options+" -strict -2 out.mp4";
+				String cmd = "ffmpeg -y -i big_buck_bunny_1_minute.mp4" 
+						+ options + " -strict -2 out.mp4";
 				ProcessBuilder pb = new ProcessBuilder("bash", "-c", cmd);
 				try {
 					Process process = pb.start();
@@ -49,33 +50,33 @@ public class VideoManipulator extends BorderPane {
 				} catch (IOException | InterruptedException e) {}
 				Platform.runLater(() -> asynchronousFinish());
 			}
-		
+
 		});
-		
+
 		worker.setDaemon(false);
-		
+
 		worker.start();
 	}
-	
+
 	private void asynchronousFinish() {
 		submit.setDisable(false);
 	}
-	
+
 	private class ControlPanel extends GridPane {
-		
+
 		private RadioButton negate;
-		
+
 		private Slider fps;
-		
+
 		private TextField text;
-		
+
 		private ControlPanel() {
 			negate = new RadioButton("Negate");
 			fps = new Slider(1, 24, 24);
 			fps.setShowTickLabels(true);
 			text = new TextField();
 			text.setPrefWidth(100);
-			
+
 			setVgap(5);
 			setHgap(5);
 			setPadding(new Insets(5));
@@ -84,14 +85,14 @@ public class VideoManipulator extends BorderPane {
 			add(fps, 1, 0);
 			add(text, 2, 0);
 		}
-		
+
 		private String getSettings() {
 			StringBuilder sb = new StringBuilder();
-			if (negate.isSelected()) {
-				sb.append(" -vf negate");
-			}
 			sb.append(" -vf \"drawtext=/usr/share/fonts/truetype/abyssinica/AbyssinicaSIL-R.ttf"
 					+ ":text=" + text.getText() +":fontsize=30:x=(w-text_w)/2:y=(h-text_h)/2\"");
+			if (negate.isSelected()) {
+				sb.append(",negate");
+			}
 			sb.append(" -r " + fps.getValue());
 			return sb.toString();
 		}
