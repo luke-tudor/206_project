cd2315268e6de66fd1a209d90793867f213f1bc3
Luke Tudor <ltud719@aucklanduni.ac.nz>
2016-10-10 16:42:07 +1300

    removed ability to draw text

--- src/spellAid/ui/video/VideoManipulator.java
+++ src/spellAid/ui/video/VideoManipulator.java
@@ -15,7 +15,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.Slider;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.util.Duration;
@@ -72,10 +71,8 @@ public class VideoManipulator extends VBox {
 			@Override
 			public void run() {
 				String options = cPanel.getSettings();
-				//String cmd = "ffmpeg -y -i big_buck_bunny_1_minute.mp4" 
-				//		+ options + " -strict -2 out.mp4";
-				String cmd = "./ffmpeg -y -i big_buck_bunny_1_minute.mp4" 
-								+ options + " -strict -2 out.mp4";
+				String cmd = "ffmpeg -y -i big_buck_bunny_1_minute.mp4" 
+						+ options + " -strict -2 out.mp4";
 				ProcessBuilder pb = new ProcessBuilder("bash", "-c", cmd);
 				try {
 					Process process = pb.start();
@@ -112,16 +109,12 @@ public class VideoManipulator extends VBox {
 
 		private Slider fps;
 
-		private TextField text;
-
 		private ControlPanel() {
 			labels = new Label[]{new Label("Rendering Progress:"), new Label("Invert Colour:"),
-					new Label("Change Framerate:"), new Label("Add text:")};
+					new Label("Change Framerate:")};
 			negate = new RadioButton();
 			fps = new Slider(1, 24, 24);
 			fps.setShowTickLabels(true);
-			text = new TextField();
-			text.setPrefWidth(100);
 
 			setVgap(5);
 			setHgap(5);
@@ -134,16 +127,13 @@ public class VideoManipulator extends VBox {
 			add(progressBar, 1, 0);
 			add(negate, 1, 1);
 			add(fps, 1, 2);
-			add(text, 1, 3);
 		}
 
 		private String getSettings() {
 			StringBuilder sb = new StringBuilder();
-			//sb.append(" -vf \"drawtext=/usr/share/fonts/truetype/abyssinica/AbyssinicaSIL-R.ttf"
-			//		+ ":text=" + text.getText() +":fontsize=30:x=(w-text_w)/2:y=(h-text_h)/2\"");
-			sb.append(" -vf \"drawtext=DejaVuSans.ttf" + ":text=" + text.getText() +":fontsize=30:x=(w-text_w)/2:y=(h-text_h)/2\"");
 			if (negate.isSelected()) {
-				sb.append(",negate");
+				//sb.append(",negate");
+				sb.append(" -vf negate");
 			}
 			sb.append(" -r " + fps.getValue());
 			return sb.toString();
