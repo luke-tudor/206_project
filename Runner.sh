250bdc50cc5761ab8b100fa7cb9bf16119b3987a
Luke Tudor <ltud719@aucklanduni.ac.nz>
2016-10-22 02:59:42 +1300

    added error handling to script file

--- Runner.sh
+++ Runner.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # For UG4
-/usr/lib/jvm/jdk1.8.0_91/bin/java -cp bin spellAid.ui.SpellingAid
+/usr/lib/jvm/jdk1.8.0_91/bin/java -cp bin spellAid.ui.SpellingAid 2>/dev/null
 
 # For my computer
-/usr/lib/jvm/jdk1.8.0_102/bin/java -cp bin spellAid.ui.SpellingAid
+/usr/lib/jvm/jdk1.8.0_102/bin/java -cp bin spellAid.ui.SpellingAid 2>/dev/null
 
 
